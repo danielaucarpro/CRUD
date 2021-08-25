e163b94bce1a5470cd26e3b74c65d8a873b11083
Daniel Aucar Felipe <danielaucarpro@gmail.com>
2021-08-24 21:55:11 -0700

    fixed path bug, created a pug file

--- app.js
+++ app.js
@@ -4,30 +4,37 @@ const pug = require('pug');
 const path = require('path');
 const fs = require('fs');
 
+//creating app reference
 const app = express();
 
-const database = [];
+//empty array to store new users
+const users = [];
 
+//setting express to use pug processor and using pug files inside views folder
 app.set('view engine', 'pug');
 app.set('views', 'views');
-
-app.use(express.static(path.join(__dirname, 'public')));
 
 //CRUD
 
 //create
-app.post('/', (req, res, next)=>{
-    res.render('create');
+app.get('/', (req, res, next) => {
+    res.render('create', {database: users});
 });
 
 //read
-app.get();
+app.get('/read', (req, res, next) => {
+    res.send(req.body);
+});
 
 //update
-app.put();
+app.put('/update');
 
 //delete
-app.delete();
+app.delete('/delete');
+
+app.use((req, res, next) => {
+    res.status(404).send('<center><h1>404 PAGE NOT FOUND</h1></center>');
+});
 
 const port = 4200;
 app.listen(port);
