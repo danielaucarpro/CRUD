be58385ce460a14e830fcb823fbcc1bf3d7ccf29
Daniel Aucar Felipe <danielaucarpro@gmail.com>
2021-08-24 20:58:38 -0700

    added pug, changed the main file on package json

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 //importing core modules
 const express = require('express');
+const pug = require('pug');
+const path = require('path');
 const fs = require('fs');
 
 const app = express();
 
+const database = [];
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//CRUD
+
+//create
+app.post('/', (req, res, next)=>{
+    res.render('create');
+});
+
+//read
+app.get();
+
+//update
+app.put();
+
+//delete
+app.delete();
+
+const port = 4200;
+app.listen(port);
